8e45d07e821e96ce25c20669a8c6f3b4f9cfae58
Pewdiepie <noelleroy720@gmail.com>
2018-08-26 11:49:33 +0530

    user pics inserted

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
+use App\Photo;
 use App\User;
 
 use App\Role;
@@ -40,7 +41,21 @@ class AdminUsersController extends Controller
 
     public function store(UserRequest $request){
 
-        User::create($request->all());
+        $input = $request->all();
+
+
+        if ($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['path'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+
+//        User::create($request->all());
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
 
         return redirect('/admin/users');
 
